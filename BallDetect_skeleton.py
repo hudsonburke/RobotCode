58a10411d0059d0cca46ea1db4531eed52b384ed
hudsonburke <hpb7kr@virginia.edu>
2021-11-08 15:08:29 -0500

    Ball Detect update: get depth

--- BallDetect_skeleton.py
+++ BallDetect_skeleton.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 from math import pi
 import roslib
-#roslib.load_manifest('my_package')
+# roslib.load_manifest('my_package')
 import cv2
 import rospy
 import sys
@@ -13,6 +13,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import PointCloud2
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovariance
 from geometry_msgs.msg import Point
@@ -26,70 +27,75 @@ bridge = CvBridge()
 
 
 class Tracker3D():
-    
-    center_pixel = (320,240)
+
+    center_pixel = (320, 240)
 
     ################
     # <raw_image> : Raw image topic
-    # <point_cloud> : POint cloud from depth camera
+    # <point_cloud> : Point cloud from depth camera
     ################
-    
-    def __init__(self,img_topic_name="/usb_cam/image_raw", depth_topic_name="/camera/depth/color/points", see_image=False): #double check the depth topic name in rviz when this done
-        
-        self.image_sub = rospy.Subscriber(img_topic_name,Image,self.image_cb)
-        self.depth_sub = rospy.Subscriber(depth_topic_name,Image,self.depth_cb)
 
-        self.ballloc_pixel = [0,0]
-        self.ballloc_xyz = [0,0,0]
+    # double check the depth topic name in rviz when this done
+    def __init__(self, img_topic_name="/d400/color/image_raw", depth_topic_name="/d400/depth/color/points", see_image=False):
+
+        self.image_sub = rospy.Subscriber(img_topic_name, Image, self.image_cb)
+        self.depth_sub = rospy.Subscriber(depth_topic_name, PointCloud2, self.depth_cb)
+        self.marker_pub = rospy.Publisher("visualization_marker", Marker)
+        self.ballloc_pixel = [0, 0]
+        self.ballloc_xyz = [0, 0, 0]
 
         self.cv_image = None
         self.depth_image = None
         self.K = []
-        self.Rt = np.array([[1,0,0],[0,0,1],[0,-1,-0]])
+        self.Rt = np.array([[1, 0, 0], [0, 0, 1], [0, -1, -0]])
         self.mask = None
-        self.center = None
 
         self.listener = tf.TransformListener()
-        
-        
 
         # Wait for messages to be published on image and depth topics
         print("Waiting for image and depth topic")
-        rospy.wait_for_message(img_topic_name,Image)
-        rospy.wait_for_message(depth_topic_name,Image)
+        rospy.wait_for_message(img_topic_name, Image)
+        rospy.wait_for_message(depth_topic_name, Image)
         print("-----> Messages received")
 
         self.rate = rospy.Rate(20)
-
 
     def get_depth(self):
         # Function to get depth of the ball
         pass
 
-
     def get_xyz(self):
         # Function to compute the x,y,z coordinates of the ball
         pass
-
+        
 
     def pub_viz(self):
         # Publish the marker for the ball
-        pub = rospy.Publisher('markedImage', Image, queue_size=100)
-        #rospy.init_node('pub_viz', anonymous=True)
-        #while not rospy.is_shutdown():
-        #    pub.publish(self.cv_image, Image)
-        
+        while not rospy.is_shutdown():
+            marker = Marker()
+            marker.type = marker.SPHERE
+            marker.action = marker.ADD
+            marker.pose.position.x = self.ballloc_xyz[0]
+            marker.pose.position.y = self.ballloc_xyz[1]
+            marker.pose.position.z = self.ballloc_xyz[2]
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+            self.marker_pub.publish(marker)
+            
 
-    def image_cb(self,data):
+    def image_cb(self, data):
         try:
-		    self.cv_image = bridge.imgmsg_to_cv2(data,"bgr8")
+            self.cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        cv_image_hsv = cv2.cvtColor(self.cv_image,cv2.COLOR_BGR2HSV)
-	    
+        cv_image_hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
 
-	    # Define lower and upper range of the colors to generate a mask using hsv 
+            # Define lower and upper range of the colors to generate a mask using hsv
         sensitivity = 15
         lower_green = np.array([60-sensitivity, 90, 90])
         upper_green = np.array([60+sensitivity, 255, 255])
@@ -98,10 +104,9 @@ class Tracker3D():
         # Process your mask to reduce noise
         self.mask = cv2.bitwise_and(self.cv_image, self.cv_image, mask=mask)
 
-	    # find contours in the mask and initialize the current
-	    # (x, y) center of the ball and publish the (x,y) pixel coordinates of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-	    	cv2.CHAIN_APPROX_SIMPLE)
+           # find contours in the mask and initialize the current
+           # (x, y) center of the ball and publish the (x,y) pixel coordinates of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         # only proceed if at least one contour was found
         if cnts:
@@ -109,31 +114,32 @@ class Tracker3D():
             for c in cnts:
                 # compute the center of the contour https://www.pyimagesearch.com/2016/02/01/opencv-center-of-contour/
                 M = cv2.moments(c)
-		if M["m00"] != 0:
-		 cX = int(M["m10"] / M["m00"])
-		 cY = int(M["m01"] / M["m00"])
-		 (x,y),radius=cv2.minEnclosingCircle(c)
-		 center = (int(x), int(y))
-		 radius = int(radius)
-		 #cv2.circle(self.cv_image, center, radius, (0,255,0),2)
-		 #cv2.circle(self.cv_image, center, 7, (255, 255, 255), -1)
-		else:
-		 cX, cY = 0, 0
-		 center = (0,0)
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                # (x, y), radius = cv2.minEnclosingCircle(c)
+                # self.ballloc_pixel = [int(x), int(y)]
+                # radius = int(radius)
+                #cv2.circle(self.cv_image, center, radius, (0,255,0),2)
+                #cv2.circle(self.cv_image, center, 7, (255, 255, 255), -1)
+                else:
+                    cX, cY = 0, 0
                 # draw the contour and center of the shape on the image
+                self.ballloc_pixel = [cX, cY]
                 print("cX, cY", cX, cY)
-		cv2.drawContours(self.cv_image, [c], -1, (0, 255, 0), 2)
-            cv2.circle(self.cv_image, (cX, cY), 7, (255, 255, 255), -1)    
-            self.center = center
-	cv2.imshow("Window",self.cv_image)
-	cv2.waitKey(1)
-
-		
+                cv2.drawContours(self.cv_image, [c], -1, (0, 255, 0), 2)
+            cv2.circle(self.cv_image, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.imshow("Window", self.cv_image)
+        cv2.waitKey(1)
 
 
-    def depth_cb(self,data):
-        self.depth_image=data
-
+    def depth_cb(self, data):
+        self.depth_image = data
+        for point in sensor_msgs.point_cloud2.read_points(data, skip_nans=True, uvs=self.ballloc_pixel):
+            self.ballloc_xyz[0] = point[0]
+            self.ballloc_xyz[1] = point[1]
+            self.ballloc_xyz[2] = point[2]
+        self.pub_viz()
 
 if __name__ == "__main__":
     rospy.init_node("measure_3d")
